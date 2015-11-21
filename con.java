bba1fe7eece00d3b2b7f855af61e4ced73bc6fea
nferron99 <zmxnghjk@gmail.com>
2015-11-21 15:54:52 -0500

    Update con.java

--- con.java
+++ con.java
@@ -1,24 +1,25 @@
 
-    /*----------------------------------------------------
-    //  viz CFD simulation data
-    //  coded by Ji Zhang
-    //  http://episode-hopezh.blogspot.sg
-    //  Apr 2013
-    ----------------------------------------------------*/
+    // viz CFD simulation data
+    // coded by Ji Zhang
+    // http://episode-hopezh.blogspot.sg
+    // Apr 2013
+    //===================================================================
+
     import processing.opengl.*;
     import peasy.*;
     PeasyCam cam;
     import org.gicentre.utils.colour.*; // color scheme library
-    ArrayList<Cell> cells = new ArrayList(); //current arraylist to be used in draw()
-    ArrayList<Cell> cellsTemp = new ArrayList(); //temp arraylist to store newly loaded data
-    boolean newDataLoaded = false;
+    ArrayList<Cell> cells = new ArrayList();
     boolean displayPt  = true;
+    boolean displayVec = true;
+    boolean displayValue = false;
+    boolean showText = true;
     boolean showColor = true;
     float vecScale = 1;
     int numCells;
     String csvFilePathAndName_str = "";
 
-    ////////////////////////////////////////////////////////////////
+    //===================================================================
     void setup(){
       size(1000,800,OPENGL);
       smooth(8);
@@ -26,7 +27,8 @@
       //use the following to avoid pt been shown in fixed size
       hint(ENABLE_STROKE_PERSPECTIVE);
      
-      //_____setup perspective parm
+      // ---------------------------------------------------------
+      // setup perspective parm 
       float fov  = PI/3.0;  // field of view
       float nearClip = 1;
       float farClip = 100000;
@@ -35,19 +37,83 @@
       cam = new PeasyCam(this, 200); // the smaller the var, the close the distance to the object
     }
 
-    ////////////////////////////////////////////////////////////////
+    //===================================================================
     void draw(){
-      background(0);
-      if (newDataLoaded == true){
-        cells = cellsTemp;
-        newDataLoaded = false;
-      }
+      background(0,0,30);
+
       assignPtColor(cells);
      
       for(Cell c:cells){
         if (displayPt == true){
           c.displayPt();
         }
+      }
+     
+      //_____print information on screen
+      if(showText == true){
+        pushMatrix();
+        hint(DISABLE_DEPTH_TEST);
+       
+        camera();
+        noLights();
+       
+        // 2D code
+        noStroke();
+        fill(255,80);
+        rect(0,0, width,50);
+        rect(0,height-110, width,110);
+       
+        textSize(15);
+        fill(0,255,0);
+        text("data file : " + csvFilePathAndName_str, 15,15);
+        text("num of cells : " + numCells, 15,30);
+        text("vec scale : " + nfc(vecScale,1), 15,45);
+        text("[f] choose csv file" , 15,height-90);
+        text("[p] show/hide point" , 15,height-75);
+        text("[c] B&W or color", 15,height-60);
+
+        if (cells.size() != 0) {
+          // color scheme __________________________________________________
+          // define a Kelvin color scale
+          ColourTable cTable = new ColourTable();
+          cTable.addContinuousColourRule(0,   0,   0,   255);  // pure Blue (Value,Red,Grn,Blu)
+          cTable.addContinuousColourRule(0.4, 206, 255, 255);  // Pale blue
+          cTable.addContinuousColourRule(0.6, 238, 255, 163);  // Yellow-green.
+          cTable.addContinuousColourRule(0.7, 255, 255, 0);  // Yellow.
+          cTable.addContinuousColourRule(0.8, 240, 165, 0);  // Orange.
+          cTable.addContinuousColourRule(1,   255, 0,   0);  // pure Red. 
+         
+          //_____get min and max speed from the current csv file
+          float speedMin = 0, speedMax = 0;
+          for(Cell c:cells){
+            float speed = c.speed;
+            if(speed <= speedMin){
+              speedMin = speed;
+            } else if (speed > speedMax){
+              speedMax = speed;
+            }
+          }
+         
+          // draw color legend
+          int numBand = 20;
+          int bandHeight = 20;
+          int bandWidth = 10;
+
+          for (int i=0; i<numBand; i++){
+            color c = cTable.findColour(map(i, numBand-1,0, 0,1));
+            noStroke();
+            fill(c);
+            rect(width-bandWidth*(numBand-i)-10, height-bandHeight-10, bandWidth,bandHeight);
+          }
+         
+          textSize(15);
+          fill(0,255,0);
+          text(nfc(speedMax,2) + "m/s", width-bandWidth*numBand-50, height-bandHeight-10);
+          text(0, width-10, height-bandHeight-10);
+        }
+           
+        hint(ENABLE_DEPTH_TEST);
+        popMatrix();
       }
     }
 
@@ -72,68 +138,52 @@
       }
     }
 
-    /*----------------------------------------------------
-    load CFD results .csv file and
-    assign each row of data to a cell object
-    ----------------------------------------------------*/
     void loadCFD(File selection){
      
       if (selection == null) {
         println("Window was closed or the user hit cancel."); // Do nothing.
       } else {
         csvFilePathAndName_str = selection.getAbsolutePath(); //get csv file path and name
-      
+       
         Table t = loadTable(csvFilePathAndName_str); //create a Table object to store csv file data
-      
+       
         numCells = t.getRowCount() - 6; //exclude the top 6 rows
-          
-        cellsTemp.clear(); //clear and initiate a new array list of cells for the current set of data
-      
-        float speedMin = 0, speedMax = 0; //initialize overall min and max speed values
-      
+           
+        cells = new ArrayList();
+
         //____create cell objects
         for(int i=0; i<numCells; i++){
           Cell c = new Cell();
-          cellsTemp.add(c);
-        
+          cells.add(c);
+         
           c.id = i;
-        
+         
           c.location.x = -t.getFloat(i+6, 0); // x of the poping start point
           c.location.y = -t.getFloat(i+6, 2); // get y position from the "Z" colume in csv
           c.location.z = t.getFloat(i+6, 1);  // get z position from the "Y" colume in csv
-        
+         
           c.velocity.x = -t.getFloat(i+6, 5); // get x velocity component
           c.velocity.y = -t.getFloat(i+6, 7); // get y velocity component from the "Z" colume in csv
           c.velocity.z = t.getFloat(i+6, 6);  // get z velocity component from the "Y" colume in csv
-        
-          c.speed = t.getFloat(i+6, 4); // get speed(velcocity magnitude)
-        
-          // compare current pt speed with min and max speed and replace them if qualified
-          if(c.speed <= speedMin){
-            speedMin = c.speed;
-          } else if (c.speed > speedMax){
-            speedMax = c.speed;
-          }  
+         
+          c.speed = t.getFloat(i+6, 4); // get speed(velcocity magnitude)   
         }
-      
-        //_____assign overall min and max speed to each pt
-        for(Cell c:cellsTemp){
-          c.speed_min = speedMin;
-          c.speed_max = speedMax;
-        }
-     
-        // !!! pass the temp ArrayList "cellsTemp" to "cells" which is to be iterated in draw()
-        //cells = cellsTemp;
-      
-        newDataLoaded = true;
       }
     }
 
-    /*----------------------------------------------------
-    assign color to pt based on their speed
-    as mapped to min to max speeds of the current data set
-    ----------------------------------------------------*/
-    void assignPtColor(ArrayList<Cell> cells){// !!! note the "ArrayList<Cell>" part
+    ///////////////////////////////////////////////////////////
+    void assignPtColor(ArrayList<Cell> cells){// note the ArrayList<Cell> part!!!
+      float speedMin = 0, speedMax = 0;
+
+      //_____get min and max speed from the current csv file
+      for(Cell c:cells){
+        float speed = c.speed;
+        if(speed <= speedMin){
+          speedMin = speed;
+        } else if (speed > speedMax){
+          speedMax = speed;
+        }
+      }
      
       //_____define a Kelvin color scale2
       ColourTable cTable = new ColourTable();
@@ -146,13 +196,27 @@
      
       //_____assign color to pt by its speed mapped to overall speedMin & speedMax
       for(Cell c:cells){
-        c.ptColor = cTable.findColour(map(c.speed, c.speed_min, c.speed_max, 0, 1));
-      }
-     
+        c.ptColor = cTable.findColour(map(c.speed, speedMin, speedMax, 0, 1));
+      } 
     }
 
     void keyPressed(){
       if ( key == 'f' ) {
-        selectInput("select a new .csv file", "loadCFD");
+        selectInput("select a new .csv file", "loadCFD");       
+      }
+      if ( key == 'p' ) {
+        displayPt = (displayPt == true) ? (false):(true);
+      } 
+      if ( key == '=' ) {
+        vecScale += 0.2;
+      } 
+      if ( key == '-' ) {
+        vecScale -= 0.2;
+      }
+      if ( key == 'h' ) {
+        showText = (showText == true) ? (false):(true);
+      }
+      if ( key == 'c' ) {
+        showColor = (showColor == true) ? (false):(true);
       }
     }
